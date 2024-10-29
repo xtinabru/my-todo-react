a8926707b397cd2fb54957f73f1cedd52b069261
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-10-29 23:59:02 +0200

    Part 4. Call REST API from React

--- front/todo/src/App.js
+++ front/todo/src/App.js
@@ -1,53 +1,72 @@
-import './App.css'
-import { useState } from 'react'
+import './App.css';
+import Row from './components/Row';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const url = 'http://localhost:3001';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-  const[task, setTask] = useState('')
-  const[tasks, setTasks] = useState([])
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setTasks(response.data);
+      })
+      .catch(error => {
+        alert(error.response && error.response.data && error.response.data.error ? error.response.data.error : error);
+      });
+  }, []);
 
   const addTask = () => {
-    if (task.trim() !== '') { // Check empty task
-      setTasks((prevTasks) => [...prevTasks, task]); 
-      setTask(''); 
-    }
+    axios.post(url + '/create', {
+      description: task
+    })
+      .then(response => {
+        setTasks([...tasks, { id: response.data.id, description: task }]);
+        setTask('');
+      })
+      .catch(error => {
+        alert(error.response && error.response.data && error.response.data.error ? error.response.data.error : error);
+      });
   };
 
-  const deleteTask = (deleted) => {
-    const withoutRemoved = tasks.filter((item) => item !== deleted)
-    setTasks(withoutRemoved)
-  }
+  const deleteTask = (id) => {
+    axios.delete(url + '/delete/' + id)
+      .then(() => {
+        const withoutRemoved = tasks.filter((item) => item.id !== id);
+        setTasks(withoutRemoved);
+      })
+      .catch(error => {
+        alert(error.response && error.response.data && error.response.data.error ? error.response.data.error : error);
+      });
+  };
 
   return (
-  <div id="container">
-    <h3>Todos</h3>
-    <form>
-      <input 
-      placeholder='Add new task'
-      value={task}
-      onChange={e => setTask(e.target.value)}
-      onKeyDown={e => {
-        if (e.key === 'Enter'){
-          e.preventDefault()
-          addTask()
+    <div id="container">
+      <h3>Todos</h3>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          placeholder="Add new task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              addTask();
+            }
+          }}
+        />
+      </form>
+      <ul>
+        {
+          tasks.map(item => (
+            <Row key={item.id} item={item} deleteTask={deleteTask} />
+          ))
         }
-      }}
-      />
-    </form>
-    <ul>
-      {
-        tasks.map((item, index)=> (
-          <li key={index}>{item}
-          <button className='delete-button' onClick={() => deleteTask(item)}>
-            Delete
-          </button>
-          </li>
-          
-        ))
-      }
-
-    </ul>
-  </div>
+      </ul>
+    </div>
   );
 }
 
---
+++ front/todo/src/components/Row.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Row({item, deleteTask}) {
+  return (
+    <div>
+      <li key={item.id}>{item.description}
+          <button className='delete-button' onClick={() => deleteTask(item.id)}>
+            Delete
+          </button>
+          </li>
+    </div>
+  )
+}
