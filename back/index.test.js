c7204f81f68ca99133629a45f16188cc4854fd03
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-11-02 17:10:36 +0200

    Part 6. Register endpoint unique e-mail

--- back/index.test.js
+++ back/index.test.js
@@ -77,7 +77,7 @@ describe('DELETE task', () => {
 
 // test for register endpoint
 describe('POST register', () => {
-  const email = 'register@foo.com'
+  const email = 'register${Date.now()}@foo.com'
   const password = 'register123'
   it('should register with valid email and password', async () => {
     const response = await fetch(base_url + 'user/register', {
