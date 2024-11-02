fce275e83f2421346ee7a032b5697c831c1a17e1
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-11-02 04:52:59 +0200

    Part 6. Add account table to database

--- back/db_setup.sql
+++ back/db_setup.sql
@@ -1,7 +1,16 @@
-create table task {
+drop table if exists task;
+drop table if exists account;
+
+create table account (
+  id serial primary key,
+  email varchar(50) unique not null,
+  password varchar(255) not null
+);
+
+create table task (
   id serial primary key,
   description varchar(255) not null
-};
+);
 
 insert into task (description) values ('My test task');
 insert into task (description) values ('My another test task');
